73552d5b0b0a7f5043a217d4e2f4a5fe6ff34631
Jay Wang <jaywang007@yahoo.com>
2017-11-21 09:45:02 -0500

    add router, trying to complete login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { HttpModule } from '@angular/http';
@@ -11,6 +11,11 @@ import { NavbarComponent } from './navbar/navbar.component';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { UserService } from './login/user.service';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'login', pathMatch: 'full'},
+  { path: 'login', component: LoginComponent }
+]
 
 @NgModule({
   declarations: [
@@ -25,8 +30,7 @@ import { UserService } from './login/user.service';
     FormsModule,
     HttpModule,
     HttpClientModule,
-    RouterModule.forRoot([
-      ])
+    RouterModule.forRoot(routes)
   ],
   providers: [DatePipe, UserService],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -32,7 +32,6 @@ export class LoginComponent implements OnInit {
           this.router.navigateByUrl('/user-home');
           localStorage.setItem('currentUser', JSON.stringify(res));
         }
-        });
+      });
   }
-
 }
