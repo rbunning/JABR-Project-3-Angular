90bc3e82055bc74f4eb9a582d76dcf031b6360ef
William Deala <wdeala@gmail.com>
2017-11-27 13:12:10 -0500

    removed services - they belong in other repo

--- ChartService/src/main/java/com/revature/project3/dao/ChartRepository.java
+++
@@ -1,9 +0,0 @@
-package com.revature.project3.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.revature.project3.bean.Chart;
-
-public interface ChartRepository extends CrudRepository<Chart, Integer> {
-
-}
