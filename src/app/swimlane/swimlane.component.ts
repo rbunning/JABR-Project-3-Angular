fb2541f5e52e20d43bb718006244f962130eb52e
Jay Wang <jaywang007@yahoo.com>
2017-11-28 16:56:33 -0500

    swimlane almost completed

--- src/app/swimlane/swimlane.component.ts
+++ src/app/swimlane/swimlane.component.ts
@@ -14,15 +14,32 @@ export class SwimlaneComponent implements OnInit {
   private currentBoard= JSON.parse(localStorage.getItem('currentBoardForLanes'));
   currentBoardId = this.currentBoard[0].boardId;
   currentBoardName = this.currentBoard[0].boardName;
-  name = 'really?';
+
+  lanes = [
+    "Backlog",
+    "To Do",
+    "In Progress",
+    "Test",
+    "Verify",
+    "Done"
+  ]
   stories: Story[];
 
   constructor(
+    private router: Router,
     private swimlaneService: SwimlaneService
     ) { }
 
   ngOnInit() {
-    console.log("This is currentBoard", this.currentBoard);
-    console.log(this.currentBoardId + ' ' + this.currentBoardName);
+    this.displayAllStories();
+  }
+
+  displayAllStories(): void {
+    this.swimlaneService.getAllStories(this.currentBoardId).subscribe(
+      res => {
+        this.stories = res;
+        console.log("Stories: ", this.stories);
+        localStorage.setItem('curentStories', JSON.stringify(res));
+      })
   }
 }
